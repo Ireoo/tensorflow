d5a3310e525c5bc729f8431c158bd394eb0f24f3
S2 <s@ireoo.com>
2019-09-14 02:39:17 +0800

    update

--- init.sh
+++ init.sh
@@ -3,14 +3,16 @@ apt upgrade -y
 
 apt install wget git -y
 
-curl -o cuda.deb https://github.com/Ireoo/tensorflow/releases/download/cnn/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
-dpkg -i cuda.deb
+curl cuda.deb https://github.com/Ireoo/tensorflow/releases/download/cnn/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb -O -J -L
+curl cudnn.deb https://github.com/Ireoo/tensorflow/releases/download/cnn/libcudnn7_7.6.3.30-1+cuda10.0_amd64.deb -O -J -L
 
+dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
+apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
 apt update
 apt install cuda-10-0
 
-curl -o cudnn.deb https://github.com/Ireoo/tensorflow/releases/download/cnn/libcudnn7_7.6.3.30-1+cuda10.0_amd64.deb
-dpkg -i cudnn.deb
+
+dpkg -i libcudnn7_7.6.3.30-1+cuda10.0_amd64.deb
 
 apt install python3-pip -y
 
