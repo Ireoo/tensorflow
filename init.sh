92a843850705cf4f13d4527c1d2986c952ee5de5
S2 <s@ireoo.com>
2019-09-14 02:10:13 +0800

    update

--- init.sh
+++ init.sh
@@ -3,12 +3,13 @@ apt upgrade -y
 
 apt install wget git -y
 
-wget https://developer.nvidia.com/compute/machine-learning/cudnn/secure/7.6.3.30/Production/10.0_20190822/Ubuntu18_04-x64/libcudnn7_7.6.3.30-1%2Bcuda10.0_amd64.deb
 wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64 -o cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
 
 dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
 apt update
 apt install cuda-10-0
+
+wget https://github.com/Ireoo/tensorflow/releases/download/cnn/libcudnn7_7.6.3.30-1+cuda10.0_amd64.deb
 dpkg -i libcudnn7_7.6.3.30-1+cuda10.0_amd64.deb
 
 apt install python3-pip -y
