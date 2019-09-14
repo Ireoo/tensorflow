a72f04815cef71c8bc4c2fc405ad586507f41b54
S2 <s@ireoo.com>
2019-09-14 17:26:16 +0800

    update

--- captcha/captcha_train.py
+++ captcha/captcha_train.py
@@ -19,9 +19,7 @@ def run_train():
     with tf.Graph().as_default():
         images, labels = captcha.inputs(
             train=True, batch_size=FLAGS.batch_size)
-
         logits = captcha.inference(images, keep_prob=1)
-
         loss = captcha.loss(logits, labels)
 
         train_op = captcha.training(loss)
@@ -31,40 +29,45 @@ def run_train():
         init_op = tf.group(tf.global_variables_initializer(),
                            tf.local_variables_initializer())
 
-        sess = tf.Session()
+        with tf.Session() as sess:
 
-        sess.run(init_op)
+            sess.run(init_op)
 
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
-        try:
-            step = 0
-            while not coord.should_stop():
-                start_time = time.time()
-                _, loss_value = sess.run([train_op, loss])
-                duration = time.time() - start_time
-                if step % 10 == 0:
-                    print('>> Step %d run_train: loss = %.2f (%.3f sec)' % (step, loss_value,
-                                                                            duration))
-                if step % 100 == 0:
-                    print('>> %s Saving in %s' %
-                          (datetime.now(), FLAGS.checkpoint))
-                    saver.save(sess, FLAGS.checkpoint, global_step=step)
-                step += 1
-        except Exception as e:
-            print('>> %s Saving in %s' % (datetime.now(), FLAGS.checkpoint))
-            saver.save(sess, FLAGS.checkpoint, global_step=step)
-            coord.request_stop(e)
-        finally:
-            coord.request_stop()
-        coord.join(threads)
-        sess.close()
+            # load DATA
+            saver.restore(sess, tf.train.latest_checkpoint(
+                FLAGS.checkpoint_dir))
+
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+            try:
+                step = 0
+                while not coord.should_stop():
+                    start_time = time.time()
+                    _, loss_value = sess.run([train_op, loss])
+                    duration = time.time() - start_time
+                    if step % 10 == 0:
+                        print('>> Step %d run_train: loss = %.2f (%.3f sec)' % (step, loss_value,
+                                                                                duration))
+                    if step % 100 == 0:
+                        print('>> %s Saving in %s' %
+                              (datetime.now(), FLAGS.checkpoint))
+                        saver.save(sess, FLAGS.checkpoint, global_step=step)
+                    step += 1
+            except Exception as e:
+                print('>> %s Saving in %s' %
+                      (datetime.now(), FLAGS.checkpoint))
+                saver.save(sess, FLAGS.checkpoint, global_step=step)
+                coord.request_stop(e)
+            finally:
+                coord.request_stop()
+            coord.join(threads)
+        # sess.close()
 
 
 def main(_):
-    if tf.gfile.Exists(FLAGS.train_dir):
-        tf.gfile.DeleteRecursively(FLAGS.train_dir)
-    tf.gfile.MakeDirs(FLAGS.train_dir)
+    # if tf.gfile.Exists(FLAGS.train_dir):
+    #     tf.gfile.DeleteRecursively(FLAGS.train_dir)
+    # tf.gfile.MakeDirs(FLAGS.train_dir)
     run_train()
 
 
